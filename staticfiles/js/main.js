fb9034b7a8a0e129e261cdcc2883e30328060736
Yeasin Kabir Joy <52117661+YeasinKabirJoy@users.noreply.github.com>
2023-01-06 06:01:16 +0600

    collectstatic

--- staticfiles/js/main.js
+++ staticfiles/js/main.js
@@ -11,3 +11,35 @@ if(searchForm){
         });
     }
 }
+
+
+let tags = document.getElementsByClassName("project-tag")
+
+if(tags){
+    for(let i=0; tags.length>i;i++){
+            tags[i].addEventListener('click',(e)=>{
+                tagId=e.target.dataset.tag
+                blogId=e.target.dataset.blog
+
+                fetch(`http://127.0.0.1:8000/api/remove-tag/`,{
+                method:'DELETE',
+                headers:{
+                    'Content-Type':'application/json',
+                },
+                body:JSON.stringify({'tag':tagId,"blog":blogId})
+
+                })
+                .then(response => response.json())
+                .then(data =>{
+                    e.target.remove()
+                })
+                })
+        }
+}
+
+
+const date = new Date()
+let date_time = document.getElementById("date-time")
+let year = date.getFullYear();
+
+date_time.innerHTML = `&#169; BlogEx | ${year}`
